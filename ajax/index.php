3ea403de1c0c1a7fe103a3cad7c19b165bd70c3d
kaizer14th <kaizer14th2@gmail.com>
2013-04-23 09:30:37 +0300

    fi

--- ajax/index.php
+++ ajax/index.php
@@ -1,10 +1,3 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
 <!DOCTYPE HTML>
 <html>
   <head>
@@ -12,6 +5,12 @@
     <script type="text/javascript" src="jquery-1.9.1.min.js"></script>
   </head>
   <body>
+      
+      <div id="post">
+      </div>
+      <div id="changeForm">
+          
+      </div>
       <script language="javascript" type="text/javascript">
         $(document).ready(function(){
 
@@ -23,48 +22,6 @@
             });
 
         });
-        var id;
-        $(".clic").click(function(){
-        alert("fff");
-   
-        });
-        $(".click").click(function(){
-            console.log(11111111111);
-            $("#changeForm").css({"display" : "block"});
-            id = $(this).attr('value');
-            $.ajax({
-              url: 'response.php',
-              success: function(data) {
-                $('#changeForm').html(data);
-              }
-            });
-
-        });
     </script>
-      <div id="post">
-      <?PHP 
-//        $post = mysql_query("SELECT * FROM messages"); 
-//        while ($row = mysql_fetch_assoc($post)) {
-//        $name = $row['name'];
-//        $id = $row['id'];
-//        $disc = $row['disc'];
-//        // Writing bloc
-//        echo "<div class='addField com'>
-//                
-//                <div class='nameM'>". $name ."</div><br />
-//                <div class='textM h'>". $disc ."</div>
-//                <form Method='POST' name='changeForm'>
-//                <input type='hidden' value='". $id ."'>
-//                </form>
-//
-//";
-//                
-//        }
-        ?>
-      </div>
-      <div id="changeForm">
-          
-      </div>
-    <div class="click"></div>
   </body>
 </html>
--- ajax/response.php
+++ ajax/response.php
@@ -1,6 +1,6 @@
 <?php
 include 'connect.php';
-
+if (empty($_GET['id'])) {
  $post = mysql_query("SELECT * FROM post"); 
 
         while ($row = mysql_fetch_assoc($post)) {
@@ -12,36 +12,55 @@ include 'connect.php';
                 
                 <div class='nameM'>". $name ."</div><br />
                 <div class='textM h'>". $disc ."</div>
-                <div class='click' value='". $id ."'>Change</div>
+                <span class='click' value='". $id ."'>Change</span>
                 </div>";
         }
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+} else {
+    $uid = $_GET['id'];
+    $post = mysql_query("SELECT * FROM post WHERE id=". $uid); 
+
+        $row = mysql_fetch_assoc($post);
+        $name = $row['name'];
+        $id = $row['id'];
+        $disc = $row['disc'];
+        
+        echo "<div class='addField com'>
+                <form action='updateForm.php' id='form' method='post'>
+                    <input type='textfield' class='nameM' name='name' value='". $name ."'><br />
+                    <input type='textfield' class='textM h' name='disc' value='". $disc ."'><br />
+                    <input type='hidden' class='id' name='id' value='". $id ."'>
+                    <input type='submit' class='update' value='update'>
+                </form>
+              </div>";
+        
+}
+
 ?>
 
 <script language="javascript" type="text/javascript">
-        $(document).ready(function(){
 
-            $.ajax({
-              url: 'response.php',
-              success: function(data) {
-                $('#post').html(data);
-              }
-            });
-
-        });
         var id;
         $(".click").click(function(){
             $("#changeForm").css({"display" : "block"});
             id = $(this).attr('value');
             $.ajax({
-              url: 'responseCH.php',
+              url: 'response.php?id='+id,
               success: function(data) {
                 $('#changeForm').html(data);
               }
             });
+
+        });
+        $('#form').submit(function() {
+        
+            var $form = $( this ),
+            name = $form.find( 'input[name="name"]' ).val(),
+            disc = $form.find( 'input[name="disc"]' ).val(),
+            id = $form.find( 'input[name="id"]' ).val(),
+            url = $form.attr( 'action' );
+
+            // Send the data using post
+            var posting = $.post( url, { n: name, d: disc, i: id } );
 
         });
     </script>
